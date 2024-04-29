f559798d2c97a7db1afcdf108d5ca3e78cd55e4f
rkgith01 <rajkapadia34@gmail.com>
2024-04-29 11:31:53 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# **25 + 5 Pomodoro Timer**
+
+This project is a 25+5 clock implemented using React.js, CSS, and HTML. It functions as a Pomodoro timer, providing users with a tool for managing their work and break intervals efficiently.
+
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/rkgith01/pen/dyLrLvB](https://codepen.io/rkgith01/pen/dyLrLvB).
+
+
+
+**Features:**
+- **Session Length Control:** Users can adjust the duration of both work sessions (session length) and break sessions (break length) using the provided controls.
+- **Timer Display:** The application prominently displays the current session type (either "Session" or "Break") and the time remaining in the session.
+- **Start/Stop Functionality:** Users can easily start or pause the timer by clicking the "Start/Stop" button.
+- **Reset Functionality:** To reset the timer and return to the default settings of a 25-minute work session and a 5-minute break session, users can click the "Reset" button.
+- **Audio Notification:** An audible alert (a beep sound) is played when the timer switches between work and break sessions or when the timer reaches zero.
+
+**Technologies Used:**
+- **React.js:** The application is developed using React.js, a popular JavaScript library for building user interfaces. React enables the creation of interactive and responsive components.
+- **CSS:** Cascading Style Sheets are utilized to style the user interface, ensuring a visually appealing and intuitive design.
+- **HTML:** HTML is used to structure the elements of the application and define their behavior.
+
+**Usage:**
+1. Adjust the session length and break length using the respective increment and decrement buttons.
+2. Click the "Start/Stop" button to initiate or pause the timer.
+3. During a work session, the timer will count down until reaching zero, signaling the end of the session with an audio notification.
+4. The timer will automatically transition to a break session, allowing users to take a short break.
+5. After the break session, the timer resets to the next work session, and the cycle repeats.
+6. To reset the timer to its initial settings or make changes to the session lengths, simply click the "Reset" button.
+
+**Note:** This Pomodoro timer is a basic implementation of the technique and can be customized or enhanced with additional features based on user preferences and requirements. Its simplicity and versatility make it a valuable tool for managing time effectively.
---
+++ src/script.js
@@ -0,0 +1,180 @@
+import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+import * as React from "https://cdn.skypack.dev/react@17.0.1";
+
+const Pomodoro = () => {
+  const [breakLength, setBreakLength] = React.useState(5);
+  const [sessionLength, setSessionLength] = React.useState(25);
+  const [timeLeft, setTimeLeft] = React.useState(1500);
+  const [timeLabel, setTimeLabel] = React.useState("New Session");
+  const [titleChanged, setTitleChanged] = React.useState(false);
+  const [playPomodoro, setPlayPomodoro] = React.useState(false);
+
+  const checkTimeOut = setTimeout(() => {
+    if (timeLeft && playPomodoro) {
+      setTimeLeft(timeLeft - 1);
+    }
+  }, 1000);
+
+  const increaseBreak = () => {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1);
+    }
+  };
+
+  const decreaseBreak = () => {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1);
+    }
+  };
+
+  const incrementSession = () => {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+      setTimeLeft(timeLeft + 60);
+    }
+  };
+
+  const decrementSession = () => {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+      setTimeLeft(timeLeft - 60);
+    }
+  };
+
+  const clickReset = () => {
+    clearTimeout(checkTimeOut);
+    setPlayPomodoro(false);
+    setTimeLeft(1500);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimeLabel("New Session");
+    setTitleChanged(false);
+    const audio = document.getElementById("beep");
+    audio.pause();
+    audio.currentTime = 0;
+  };
+
+  const clickPlay = () => {
+    setTitleChanged(true);
+    clearTimeout(checkTimeOut);
+    setPlayPomodoro(!playPomodoro);
+  };
+
+  const resetTimer = () => {
+    const audio = document.getElementById("beep");
+    if (!timeLeft && timeLabel === "New Session") {
+      setTimeLeft(breakLength * 60);
+      setTimeLabel("break");
+      audio.play();
+    }
+    if (!timeLeft && timeLabel === "break") {
+      setTimeLeft(sessionLength * 60);
+      setTimeLabel("New Session");
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  };
+
+  const timer = () => {
+    playPomodoro ? checkTimeOut && resetTimer() : clearTimeout(checkTimeOut);
+  };
+
+  React.useEffect(() => {
+    timer();
+  }, [playPomodoro, timeLeft, checkTimeOut]);
+
+  const formateTime = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    return `${minutes < 10 ? "0" : ""}${minutes}:${
+      seconds < 10 ? "0" : ""
+    }${seconds}`;
+  };
+
+  const mainTitle = timeLabel === "New Session" ? "Session" : "Break";
+
+  return (
+    <>
+      <div class="logo">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="256"
+          height="256"
+          viewBox="0 0 100 100"
+        >
+          <rect width="100" height="100" rx="20" fill="#4a90e2" />
+          <path
+            d="M40.20 86L40.20 86L24.70 86Q22.80 86 21.95 85.15Q21.10 84.30 21.10 82.40L21.10 82.40L21.10 17.60Q21.10 15.70 21.95 14.85Q22.80 14 24.70 14L24.70 14L55.30 14Q68.90 14 73.90 19.40Q78.90 24.80 78.90 33.50L78.90 33.50L78.90 46.30Q78.90 55 73.90 60.40Q68.90 65.80 55.30 65.80L55.30 65.80L43.80 65.80L43.80 82.40Q43.80 84.30 42.95 85.15Q42.10 86 40.20 86ZM52.60 32.70L43.60 32.70L43.60 47.80L52.60 47.80Q55.50 47.80 56.40 46.30Q57.30 44.80 57.30 42.90L57.30 42.90L57.30 37.60Q57.30 35.70 56.40 34.20Q55.50 32.70 52.60 32.70L52.60 32.70Z"
+            fill="#000000"
+          />
+        </svg>
+      </div>
+      <div className="container">
+        <h2 className={titleChanged ? "title-changed" : ""}>
+          {titleChanged ? "Its Pomodoro Time" : "25 + 5 Clock"}
+        </h2>
+        <div className="length-container">
+          <div>
+            <h3 id="session-label">Session Length</h3>
+            <div>
+              <button
+                disabled={playPomodoro}
+                onClick={incrementSession}
+                id="session-increment"
+              >
+                +
+              </button>
+              <strong id="session-length">{sessionLength}</strong>
+              <button
+                disabled={playPomodoro}
+                onClick={decrementSession}
+                id="session-decrement"
+              >
+                -
+              </button>
+            </div>
+          </div>
+          <div>
+            <h3 id="break-label">Break Length</h3>
+            <div>
+              <button
+                disabled={playPomodoro}
+                onClick={increaseBreak}
+                id="break-increment"
+              >
+                +
+              </button>
+              <strong id="break-length">{breakLength}</strong>
+              <button
+                disabled={playPomodoro}
+                onClick={decreaseBreak}
+                id="break-decrement"
+              >
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="clock-wrapper">
+          <div className="clock">
+            <h2 id="timer-label">{mainTitle}</h2>
+            <h3 id="time-left">{formateTime()}</h3>
+          </div>
+          <button onClick={clickPlay} id="start_stop">
+            Start/Stop
+          </button>
+          <button onClick={clickReset} id="reset">
+            Reset
+          </button>
+        </div>
+        <audio
+          id="beep"
+          preload="auto"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        />
+      </div>
+    </>
+  );
+};
+
+ReactDOM.render(<Pomodoro />, document.getElementById("pomodoro"));
